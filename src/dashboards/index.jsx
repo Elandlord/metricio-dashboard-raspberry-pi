0e6d26ca131e62325e0b55a848a6c6dfcb97250b
Eric Landheer <e.landheer@mentechmedia.nl>
2018-08-21 12:58:31 +0200

    :art: Tinkering with dashboard

--- src/dashboards/index.jsx
+++ src/dashboards/index.jsx
@@ -14,11 +14,14 @@ ReactDOM.render(
   <Dashboard>
     <SparklineWidget name="DemoUsers" title="Users" format="0.00a" />
     <PingWidget name="GooglePing" title="API" />
-    <NumberWidget name="ReasonPRs" title="Pull Requests" />
-    <BuildStatusWidget name="DemoMaster" title="Build - Master" size="medium" />
+    {/* <NumberWidget name="ReasonPRs" title="Pull Requests" /> */}
+    <NumberWidget name="briqchain" title="PR MEN"/>
+    {/* <BuildStatusWidget name="DemoMaster" title="Build - Master" size="medium" /> */}
+    <BuildStatusWidget name="BriqchainBuildProduction" title="Briqchain - Master" size="medium" />
     <ProgressWidget name="DemoProgress" title="Sales Target" />
     <NumberWidget name="DemoConversion" title="Conversion" metric="%" format="0.0a" />
-    <BuildStatusWidget name="DemoDevelop" title="Build - Develop" size="medium" />
+    {/* <BuildStatusWidget name="DemoDevelop" title="Build - Develop" size="medium" /> */}
+    <BuildStatusWidget name="BriqchainBuildDevelopment" title="Briqchain - Develop" size="medium" />
   </Dashboard>,
   document.getElementById('content'),
 );
